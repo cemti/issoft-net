75a102f4cf310654b5317d2ddd9d7c63ec5cf3eb
cemti <cemirtan.cristian@outlook.com>
2024-01-19 13:38:55 +0200

    Task2 done.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        static IEnumerable<string> FindSubsequences(string input)
+        {
+            for (int i = 0; i < input.Length; ++i)
+                for (int j = i + 1; j < input.Length && input[j] != input[j - 1]; ++j)
+                    yield return input[i..(j + 1)];
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (var x in args)
+            {
+                foreach (var result in FindSubsequences(x))
+                    Console.Write($"{result} ");
+
+                Console.WriteLine();
+            }
         }
     }
 }
