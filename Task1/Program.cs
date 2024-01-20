437457a32864c50e40ea0099898bcbcb3d76e631
cemti <cemirtan.cristian@outlook.com>
2024-01-20 19:21:26 +0200

    Put the query in a method.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,17 +4,17 @@ namespace Task1
 {
     static class Program
     {
+        static IEnumerable<string> GetColors(string[] args) =>
+            from color in
+                from arg in args
+                select arg is [(>= 'a' and <= 'h') or (>= 'A' and <= 'H'), >= '1' and <= '8'] ?
+                    ((arg[0] ^ arg[1]) & 1) == 0 ? Color.Black : Color.White :
+                    throw new ArgumentOutOfRangeException(arg)
+            select color.Name;
+
         static void Main(string[] args)
         {
-            var query =
-                from color in
-                    from arg in args
-                    select arg is [(>= 'a' and <= 'h') or (>= 'A' and <= 'H'), >= '1' and <= '8'] ?
-                        ((arg[0] ^ arg[1]) & 1) == 0 ? Color.Black : Color.White :
-                        throw new ArgumentOutOfRangeException(arg)
-                select color.Name;
-
-            foreach (var color in query)
+            foreach (var color in GetColors(args))
                 Console.WriteLine(color);
         }
     }
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Task2
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
