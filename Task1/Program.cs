85a0a0a0299ef0bc7a0b675892cf5db22dbf5952
cemti <cemirtan.cristian@outlook.com>
2024-01-20 19:16:12 +0200

    LINQ + pattern matching approach.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,39 +1,21 @@
 ﻿using System.Drawing;
-using System.Text.RegularExpressions;
 
 namespace Task1
 {
-    struct Cell
-    {
-        public int X { get; }
-        public int Y { get; }
-
-        public Color Color => ((X ^ Y) & 1) == 0 ? Color.Black : Color.White;
-
-        public Cell(string str)
-        {
-            var match = Regex.Match(str, "^([a-h])([1-8])$", RegexOptions.IgnoreCase);
-            var x = match.Groups[1];
-            var y = match.Groups[2];
-
-            if (!x.Success || !y.Success)
-                throw new ArgumentOutOfRangeException(str);
-
-            X = char.ToLower(x.Value[0]) - 'a';
-            Y = y.Value[0] - '1';
-        }
-
-        public override string ToString() => $"{(char)(X + 'a')}{Y + 1}";
-    }
-
     static class Program
     {
         static void Main(string[] args)
         {
-            var query = from arg in args select new Cell(arg);
+            var query =
+                from color in
+                    from arg in args
+                    select arg is [(>= 'a' and <= 'h') or (>= 'A' and <= 'H'), >= '1' and <= '8'] ?
+                        ((arg[0] ^ arg[1]) & 1) == 0 ? Color.Black : Color.White :
+                        throw new ArgumentOutOfRangeException(arg)
+                select color.Name;
 
-            foreach (var cell in query)
-                Console.WriteLine($"{cell} -> {cell.Color.Name}");
+            foreach (var color in query)
+                Console.WriteLine(color);
         }
     }
 }
